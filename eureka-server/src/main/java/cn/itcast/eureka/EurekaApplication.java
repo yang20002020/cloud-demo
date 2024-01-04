9b5ea0b80b321dd10c27ca41b3a5951ac8e8550b
nil2000 <2628636987@qq.com>
2024-01-04 14:44:34 +0800

    Nacos  环境隔离

--- eureka-server/src/main/java/cn/itcast/eureka/EurekaApplication.java
+++ eureka-server/src/main/java/cn/itcast/eureka/EurekaApplication.java
@@ -1,5 +1,4 @@
 package cn.itcast.eureka;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
